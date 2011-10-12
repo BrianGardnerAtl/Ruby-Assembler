ae132c48a62e4fc16c6034826d1748d7aae486a1
Brian Gardner <bgardner7@gatech.edu>
2011-10-12 09:37:58 -0400

    instruction code correctly prints to the output file

--- assembler.rb
+++ assembler.rb
@@ -4,26 +4,26 @@ DATA_BITS = 32
 ADDRESSES = 2048
 
 # Store register name/number as key/value pair
-register_hash = { "zero" => 0b00000, "a0" => 0b00001, "a1" => 0b00010, "a2" => 0b00011, "a3" => 0b00100,
-                  "r5" => 0b00101, "r6" => 0b00110, "r7" => 0b00111, "t0" => 0b01000, "t1" => 0b01001,
-                  "t2" => 0b01010, "t3" => 0b01011, "t4" => 0b01100, "t5" => 0b01101, "t6" => 0b01110,
-                  "t7" => 0b01111, "s0" => 0b10000, "s1" => 0b10001, "s2" => 0b10010, "s3" => 0b10011,
-                  "s4" => 0b10100, "s5" => 0b10101, "s6" => 0b10110, "s7" => 0b10111, "r24" => 0b11000,
-                  "r25" => 0b11001, "r26" => 0b11010, "r27" => 0b11011, "r28" => 0b11100, "r29" => 0b11101,
-                  "r30" => 0b11110,"r31" => 0b11111
+register_hash = { "zero" => "00000", "a0" => "00001", "a1" => "00010", "a2" => "00011", "a3" => "00100",
+                  "r5" => "00101", "r6" => "00110", "r7" => "00111", "t0" => "01000", "t1" => "01001",
+                  "t2" => "01010", "t3" => "01011", "t4" => "01100", "t5" => "01101", "t6" => "01110",
+                  "t7" => "01111", "s0" => "10000", "s1" => "10001", "s2" => "10010", "s3" => "10011",
+                  "s4" => "10100", "s5" => "10101", "s6" => "10110", "s7" => "10111", "r24" => "11000",
+                  "r25" => "11001", "r26" => "11010", "r27" => "11011", "r28" => "11100", "r29" => "11101",
+                  "r30" => "11110","r31" => "11111"
                   }
 
 # Store insruction/opcode1 as key/value pair
-opcode1_hash = { "alur" => 0b000000, "andi" => 0b000001, "ori" => 0b000010, "hi" => 0b000011,
-                 "addi" => 0b000100, "lw" => 0b010000, "sw" => 0b010001, "jal" => 0b100000,
-                 "beq" => 0b100001, "bne" => 0b100101
+opcode1_hash = { "alur" => "000000", "andi" => "000001", "ori" => "000010", "hi" => "000011",
+                 "addi" => "000100", "lw" => "010000", "sw" => "010001", "jal" => "100000",
+                 "beq" => "100001", "bne" => "100101"
                }
 
 # Store instruction/opcode2 as key/value pair
-opcode2_hash = {  "and" => 0b000001, "or" => 0b000010, "xor" => 0b000011, "nand" => 0b000101,
-                  "nor" => 0b000110, "nxor" => 0b000111, "add" => 0b010000, "sub" => 0b010001,
-                  "eq" => 0b100001, "lt" => 0b100010, "le" => 0b100011, "ne" => 0b100101,
-                  "ge" => 0b100110, "gt" => 0b100111
+opcode2_hash = {  "and" => "000001", "or" => "000010", "xor" => "000011", "nand" => "000101",
+                  "nor" => "000110", "nxor" => "000111", "add" => "010000", "sub" => "010001",
+                  "eq" => "100001", "lt" => "100010", "le" => "100011", "ne" => "100101",
+                  "ge" => "100110", "gt" => "100111"
                }
 
 pseudo_inst = ["subi", "not", "br", "blt", "ble", "bgt", "bge", "call", "jmp", "ret"]
@@ -65,6 +65,13 @@ if(file_specified && file_exists)
   #TODO check if output file already exists
   output_file = File.new(output_file_name, "w")
 
+  #Put the file headers in the output file
+  output_file.puts "WIDTH=#{DATA_BITS};"
+  output_file.puts "DEPTH=#{ADDRESSES};"
+  output_file.puts "ADDRESS_RADIX=HEX;"
+  output_file.puts "DATA_RADIX=HEX;"
+  output_file.puts "CONTENT BEGIN"
+
   #open the specified .a32 file for reading for first pass
   input_file = File.open(source_file, "r")
 
@@ -78,58 +85,408 @@ if(file_specified && file_exists)
       # Extract the new name/address pair and remove excess white space
       name_line = line[5..-1].strip
       temp_name_var, temp_name_val = name_line.split('=')
-      name_var = temp_name_var.strip
-      name_val = temp_name_val.strip
+      name_var = temp_name_var.strip.downcase
+      name_val = temp_name_val.strip.split('0x')[1]
       name_hash[name_var] = name_val
     end
   end
   # END OF FIRST PASS
 
+#  name_hash.each do |name, val|
+#    puts "Name: #{name}, Value: #{val}"
+#  end
+
   # set the input back to the beginning of input_file
   input_file.rewind
 
-  # regex to match code sections, and instructions(normal&pseudo)
-  section_regex = /[a-zA-Z]+:/
+  # regex to match various parts of the code that must be handled differently
+  section_regex = /[a-zA-Z0-9]+:/
   inst_regex = /\s([a-zA-Z]+)/
   orig_regex = /ORIG/
+  comment_regex = /^\s+;/
 
-  line_cnt = 0
-  # SECOND PASS ->
+  #Create new .a32 file that has no pseudo instructions
+  temp_src = File.new("temp.a32", "w")
+
+  # SECOND PASS -> replace pseudo instructions in temp.a32 file
   while (line = input_file.gets)
-    # Check for the ORIG keyword to set the line_cnt var
+
+    # variable indicating whether or not to write the unmodified line to temp.a32
+    write = true
+
+    #Check if the line contains a pseudo instruction
+    if is_inst = line =~ inst_regex
+      inst_name = inst_regex.match(line)[1].strip.downcase
+      if index = pseudo_inst.index(inst_name)
+        write = false
+        opcode, regs = line.split("\s")
+        regs_arr = regs.split(",")
+#        puts "Instruction: #{inst_name}, Regs Arr: #{regs_arr}"
+        # Find out which instruction it is, and replace it with real instructions
+        case inst_name
+        when "subi"
+          new_imm = ((regs_arr[2].to_i)*-1).to_s
+          new_regs = [regs_arr[0], regs_arr[1], new_imm].join(',')
+          out_line = ["\taddi", new_regs].join(' ')
+        when "not"
+          new_regs = [regs_arr[0], regs_arr[1], regs_arr[1]].join(',')
+          out_line = ["\tnor", new_regs].join(' ')
+        when "br"
+          new_regs = ["zero","zero",regs_arr[0]].join(',')
+          out_line = ["\tbeq", new_regs].join(' ')
+        when "blt"
+          #Need to insert 2 instructions
+          new_regs = ["r6", regs_arr[0], regs_arr[1]].join(',')
+          out_line = ["\tlt", new_regs].join(' ')
+          temp_src.puts out_line
+
+          new_regs = ["r6", "zero", regs_arr[2]].join(',')
+          out_line = ["\tbne", new_regs].join(' ')
+        when "ble"
+          new_regs = ["r6", regs_arr[0], regs_arr[1]].join(',')
+          out_line = ["\tle", new_regs].join(' ')
+          temp_src.puts out_line
+
+          new_regs = ["r6", "zero", regs_arr[2]].join(',')
+          out_line = ["\tbne", new_regs].join(' ')
+        when "bgt"
+          new_regs = ["r6", regs_arr[0], regs_arr[1]].join(',')
+          out_line = ["\tgt", new_regs].join(' ')
+          temp_src.puts out_line
+
+          new_regs = ["r6", "zero", regs_arr[2]].join(',')
+          out_line = ["\tbne", new_regs].join(' ')
+        when "bge"
+          new_regs = ["r6", regs_arr[0], regs_arr[1]].join(',')
+          out_line = ["\tge", new_regs].join(' ')
+          temp_src.puts out_line
+
+          new_regs = ["r6", "zero", regs_arr[2]].join(',')
+          out_line = ["\tbne", new_regs].join(' ')
+        when "call"
+          new_regs = ["ra", regs_arr[0]].join(',')
+          out_line = ["\tjal", new_regs].join(' ')
+        when "jmp"
+          new_regs = ["r6", regs_arr[0]].join(',')
+          out_line = ["\tjal", new_regs].join(' ')
+        when "ret"
+          out_line = "\tjal r6,0(ra)"
+        end
+      end
+    end
+
+    if write
+      temp_src.puts line
+    else
+      temp_src.puts out_line
+    end
+  end
+
+  # close input_file adn temp_src
+  input_file.close
+  temp_src.close
+
+  input_file = File.open("temp.a32", "r")
+  #current memory address needed for output file
+  current_addr = 0
+  #line_count
+  line_cnt = 0
+
+  #THIRD PASS set up the section hash and their memory addresses
+  while (line = input_file.gets)
+    section_hex = (line_cnt*4).to_s(16)
+    section_hex = ("0" * (8-section_hex.length)) + section_hex
+
     if line =~ orig_regex
       orig, mem_addr = line.split(' ')
-      puts "Found ORIG address: #{mem_addr}"
+      if mem_addr =~ /0x/
+        new_cnt = (mem_addr.to_i(16))/4
+      else
+        new_cnt = mem_addr.to_i
+      end
+      line_cnt = new_cnt
     end
 
     #Check if the line indicates a new code section
     if is_section = line =~ section_regex
       sec_name = section_regex.match(line)[0]
       if sec_name
-        puts "New section: #{sec_name[0..-2]}, Line count: #{line_cnt}"
-        section_hash[sec_name[0..-2]] = line_cnt
+        section_hash[sec_name[0..-2].downcase] = section_hex[-4..-1]
       end
+      next
+    end
+
+    if is_inst = line =~ inst_regex
+      inst_name = inst_regex.match(line)[1].strip.downcase
+
+      if opcode1_hash[inst_name] || opcode2_hash[inst_name] || pseudo_inst.index(inst_name)
+        line_cnt+=1
+      end
+    end
+  end
+  input_file.close
+  #END OF THIRD PASS
+
+  section_hash.each do |name, val|
+    puts "Section: #{name}, Address: #{val}"
+  end
+
+  input_file = File.open("temp.a32", "r")
+
+  #current memory address needed for output file
+  current_addr = 0
+  #line_count
+  line_cnt = 0
+  # FINAL PASS
+  while (line = input_file.gets)
+    #variables to make the output strings more easily
+    dashes = "--"
+    at = "@"
+    hex = "0x"
+    line_hex = (line_cnt*4).to_s(16)
+    line_hex = hex + ("0" * (8-line_hex.length)) + line_hex
+    code_hex = line_cnt.to_s(16)
+    code_hex = ("0" * (8-code_hex.length)) + code_hex
+    section_hex = (line_cnt*4).to_s(16)
+    section_hex = ("0" * (8-section_hex.length)) + section_hex
+
+    inst_var, comment = line.split(";")
+
+    # String that contains the memory location and the instruction
+    inst_string = [dashes, at, line_hex, ":", inst_var].join(' ')
+
+    data_string = [code_hex, " : "].join
+
+    # check if the line is a comment
+    if line =~ comment_regex
+      next
+    end
+
+    # check if the line has the NAME keyword
+    if line =~ name_regex
+      next
+    end
+
+    # Check for the ORIG keyword to set the line_cnt var
+    if line =~ orig_regex
+      orig, mem_addr = line.split(' ')
+      #TODO figure out where new line_hex and code hex are, and where 0xDEAD mem addresses are
+      if mem_addr =~ /0x/
+        new_cnt = (mem_addr.to_i(16))/4
+      else
+        new_cnt = mem_addr.to_i
+      end
+
+      # make memory between line_cnt and new_cnt 0xDEAD
+      if (new_cnt-line_cnt) > 1
+        low_mem = line_cnt
+        high_mem = new_cnt-1
+        #Insert 0xDEAD into empty memory
+        low_str = low_mem.to_s
+        low_str = ("0" * (8-low_str.length)) + low_str
+        high_str = high_mem.to_s
+        high_str = ("0" * (8-high_str.length)) + high_str
+        dead_str = ["[", low_str, "..", high_str, "] : DEAD;"].join
+        output_file.puts dead_str
+
+        #set line_cnt to new_cnt
+        line_cnt = new_cnt
+      end
+
+      next
     end
 
     #check if the line is an instruction
     if is_inst = line =~ inst_regex
       inst_name = inst_regex.match(line)[1].strip.downcase
 
+      # variable to indicate whether or not to write to the output file
+      write = false
+
       if opcode1_hash[inst_name]
         # instruction is a primary opcode
-        puts "Primary opcode: #{inst_name}"
+        opcode, regs = line.split("\s")
+        opcode = opcode.strip.downcase
+        code_str = opcode1_hash[opcode]
+        data_bin = code_str
+
+        #Parse the registers
+        regs = regs.split(",")
+
+        #Check if instruction is I-type opcode
+        itype = ["andi", "ori", "addi", "hi"]
+        if itype.index(opcode)
+          reg0 =  register_hash[regs[0].downcase]
+          reg1 = register_hash[regs[1].downcase]
+          imm_val = regs[2].downcase
+
+          if section_hash[imm_val]
+            imm_val = section_hash[imm_val]
+          elsif imm_val =~ /0x/
+            #extract hex value
+            imm_val = imm_val.split('0x')[1]
+            if imm_val.length<4
+              imm_val = ("0" * (4-imm_val.length)) + imm_val
+            end
+          elsif imm_val =~ /\-/
+            #negative number, 2's complement it
+            imm_val = imm_val.to_i.abs
+            imm_val = (~imm_val + 1).to_s(16)[1..-1]
+#            puts "Neg Imm val: #{imm_val}"
+            if imm_val.length < 4
+              imm_val = ("1" * (4-imm_val.length)) + imm_val
+            end
+          else
+            imm_val = imm_val.to_i.to_s(16)
+            if imm_val.length<4
+              imm_val = ("0" * (4-imm_val.length)) + imm_val
+            end
+          end
+#          puts "opcode: #{data_bin}"
+#          puts "reg1: #{reg1}"
+#          puts "reg0: #{reg0}"
+#          puts "Imm val: #{imm_val}"
+
+          data_bin = [data_bin, reg1, reg0].join.to_i(2).to_s(16)
+          data_bin += imm_val
+#          puts "Data bin: #{data_bin}"
+        end
+
+        mem_inst = ["lw", "sw"]
+        if mem_inst.index(opcode)
+          reg1 = register_hash[regs[0].downcase]
+          imm_val = regs[1]
+          addr, val = imm_val.split('(')
+          val = val[0..-2]
+          if register_hash[val]
+            reg_code = register_hash[val]
+          end
+          imm_num = ""
+          if name_hash[addr.downcase]
+            imm_num = name_hash[addr.downcase][-4..-1]
+          else
+            #not a name keyword, must just be an integer
+            if addr =~ /\-/
+              #negative number, 2's complement it
+              imm_num = addr.to_i.abs
+              imm_num = (~imm_num + 1).to_s(2)[1..-1]
+              #change to hex string
+              imm_num = imm_num.to_i(2).to_s(16)
+              if imm_num.length<4
+                imm_num = ("0" * (4-imm_num.length)) + imm_num
+              end
+            else
+              #positive number
+              imm_num = addr.to_i.to_s(2)
+              if imm_num.length < 16
+                imm_num = ("0" * (16-imm_num.length)) + imm_num
+              elsif imm_num.length > 16
+                imm_num = imm_num[-16..-1]
+              end
+              imm_num = imm_num.to_i(2).to_s(16)
+              if imm_num.length<4
+                imm_num = ("0" * (4-imm_num.length)) + imm_num
+              end
+            end
+          end
+
+#          puts "opcode: #{data_bin}"
+#          puts "reg code: #{reg_code}"
+#          puts "reg1: #{reg1}"
+
+          data_bin += [reg_code, reg1].join
+          data_bin = data_bin.to_i(2).to_s(16)
+          data_bin += imm_num
+
+#          puts "Opcode: #{opcode}, mem data bin: #{data_bin}"
+        end
+
+        jumps = ["jal", "beq", "bne"]
+        if jumps.index(opcode)
+          if regs.size ==2
+            reg0 = register_hash[regs[0].downcase]
+            imm_val = regs[1]
+            if imm_val =~ /\(/
+              # immediate value is of the form 0(t0)
+              temp = imm_val.split('(')
+              reg1 = temp[1][0..-2].downcase
+              reg1 = register_hash[reg1]
+              imm_val = temp[0]
+              if imm_val.length<4
+                imm_val = ("0" * (4-imm_val.length)) + imm_val
+              end
+            else
+             reg0 = "00000"
+            end
+          elsif regs.size==3
+            reg0 = register_hash[regs[0].downcase]
+            reg1 = register_hash[regs[1].downcase]
+            imm_val = regs[2]
+          end
+          if section_hash[imm_val.downcase]
+            imm_val = section_hash[imm_val.downcase]
+            imm_val = ("0" * (4-imm_val.length)) + imm_val
+          end
+          data_bin = [data_bin, reg0, reg1].join.to_i(2).to_s(16)
+#          puts "Data Bin: #{data_bin}"
+#          puts "Reg1: #{reg1}"
+#          puts "Reg0: #{reg0}"
+#          puts "Imm val: #{imm_val}"
+
+          data_bin += imm_val
+        end
+
         line_cnt+=1
+        write = true
       end
+
       if opcode2_hash[inst_name]
-        puts "Secondary opcode: #{inst_name}"
+        opcode, regs = line.split("\s")
+        opcode = opcode.strip.downcase
+        code_str = opcode2_hash[opcode]
+        data_bin = opcode1_hash["alur"]
+        puts "Init data bin: #{data_bin}"
+
+        regs = regs.split(",")
+        regs.each do |name|
+          reg_str = register_hash[name]
+          data_bin += reg_str
+          puts "Add register: #{data_bin}"
+        end
+
+        data_bin += "00000"
+        data_bin += code_str
+        puts "add op2: #{data_bin}"
+
+        data_bin = data_bin.to_i(2).to_s(16)
+        if data_bin.length<8
+          data_bin = ("0" * (8-data_bin.length)) + data_bin
+        end
+        puts "Fin data bin: #{data_bin}"
+
         line_cnt+=1
+        write = true
       end
-      if index = pseudo_inst.index(inst_name)
-        puts "Pseudo opcode: #{inst_name}"
-        line_cnt+=1
+
+      if write
+        #make the complete instruction in hex
+        data_string += [data_bin.downcase, ";"].join
+        output_file.puts inst_string
+        output_file.puts data_string
       end
     end
   end
+
+  #TODO mark ny unused memory addresses as DEAD
+  max_mem = 2047
+  max_str = max_mem.to_s(16)
+  max_str = ("0" * (5-max_str.length)) + max_str
+  code_hex = line_cnt.to_s(16)
+  code_hex = ("0" * (5-code_hex.length)) + code_hex
+  dead_line = "[#{code_hex}..#{max_str}] : DEAD;"
+  output_file.puts dead_line
+  end_line = "END;"
+  output_file.puts end_line
 
 
   #Close both input and output file
